f301f274e54f526046dbcd0077eb5ccab74897e6
XecFardin <fardinxec007@gmail.com>
2021-10-02 10:52:14 +0530

    Api to get accessorId

--- chalicelib/helperFunctions.py
+++ chalicelib/helperFunctions.py
@@ -13,6 +13,9 @@ helperFunctions = Blueprint(__name__)
 ENV_NAME = os.environ["ENV_NAME"]
 DEBUG_ENABLE = False
 
+responseMessage = {}
+responseSubMessage = {}
+
 if ENV_NAME == 'dev':
     connection = psycopg2.connect(user="mobile_diagnostics_role",
                                   password="mobilediagnostics123",
@@ -43,16 +46,50 @@ else:
                                   host="all-test-dev.c6f7bs1lumk2.ap-south-1.rds.amazonaws.com",
                                   port="5432",
                                   database="mobile_diagnostics_dev")
+    
+def openDataBaseConnection():
+    try:
+        cursor = connection.cursor()
+        print("Database connection opened!")
+        return cursor
 
-@helperFunctions.route('/mobileDiagnosticsService/v1/hello/{name}', methods=['GET'], cors=True)
-def getHello(name):
+    except  Exception   as  e:
+        responseSubMessage['errorCode'] = 500
+        responseSubMessage['errorMessage'] = 'Internal Server Error'
+        responseMessage['type'] = 'error'
+        responseMessage['message'] = responseSubMessage
+        print(responseMessage)
+        return Response(body=responseMessage, status_code=401, headers={'Content-Type': 'application/json'})
 
-    responseMessage = {}
-    responseSubMessage = {}
+@helperFunctions.route('/mobileDiagnosticsService/v1/mobile/{mobileId}/accessorId', methods=['GET'], cors=True)
+def getAccessorId(mobileId):
+    try:
+        cursor=openDataBaseConnection()
+        cursor.execute('''SELECT "accessorId"  FROM accessors_mobiles where "mobileId" = {}'''.format(mobileId))
+        accessorId=cursor.fetchone()
+        if accessorId != None:
+            for item in accessorId:
+                accessorId=item
+            response={
+                "type":"success",
+                "message":{
+                "accessorId":accessorId
+                }
+                
+            }
+            response=json.dumps(response)
+            return Response(body=response, status_code=200, headers={'Content-Type': 'application/json'})
+        else:
+            responseSubMessage['errorCode'] = 210
+            responseSubMessage['errorMessage'] = 'accessorId not found for particular mobileId'
+            responseMessage['type'] = 'error'
+            responseMessage['message'] = responseSubMessage
+            return Response(body=responseMessage, status_code=210, headers={'Content-Type': 'application/json'})
 
-    responseSubMessage = 'hi from {}' .format(name)
-
-    responseMessage['type'] = 'success'
-    responseMessage['message'] = responseSubMessage
-
-    return Response(body=responseMessage, status_code=200, headers={'Content-Type': 'application/json'})
+        
+    except Exception as e:
+        responseSubMessage['errorCode'] = 500
+        responseSubMessage['errorMessage'] = e
+        responseMessage['type'] = 'error'
+        responseMessage['message'] = responseSubMessage
+        return Response(body=responseMessage, status_code=500, headers={'Content-Type': 'application/json'})
