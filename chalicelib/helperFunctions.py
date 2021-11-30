b00038031d802cb68158c040fb1a85886a8c2777
XecFardin <you@example.com>
2021-11-30 18:32:43 +0530

    changed password

--- chalicelib/helperFunctions.py
+++ chalicelib/helperFunctions.py
@@ -19,31 +19,31 @@ responseSubMessage = {}
 
 if ENV_NAME == 'dev':
     connection = psycopg2.connect(user="mobile_diagnostics_role",
-                                  password="mobilediagnostics123",
+                                  password="new_password",
                                   host="all-test-dev.c6f7bs1lumk2.ap-south-1.rds.amazonaws.com",
                                   port="5432",
                                   database="mobile_diagnostics_dev")
 elif ENV_NAME == 'test':
     connection = psycopg2.connect(user="mobile_diagnostics_role",
-                                  password="mobilediagnostics123",
+                                  password="new_password",
                                   host="all-test-dev.c6f7bs1lumk2.ap-south-1.rds.amazonaws.com",
                                   port="5432",
                                   database="mobile_diagnostics_test")
 elif ENV_NAME == 'sandbox':
     connection = psycopg2.connect(user="mobile_diagnostics_role",
-                                  password="mobilediagnostics123",
+                                  password="new_password",
                                   host="all-test-dev.c6f7bs1lumk2.ap-south-1.rds.amazonaws.com",
                                   port="5432",
                                   database="mobile_diagnostics_sandbox")
 elif ENV_NAME == 'prod':
     connection = psycopg2.connect(user="mobile_diagnostics_role",
-                                  password="mobilediagnostics123",
+                                  password="new_password",
                                   host="misc-prod.c6f7bs1lumk2.ap-south-1.rds.amazonaws.com",
                                   port="5432",
                                   database="mobile_diagnostics_prod")
 else:
     connection = psycopg2.connect(user="mobile_diagnostics_role",
-                                  password="mobilediagnostics123",
+                                  password="new_password",
                                   host="all-test-dev.c6f7bs1lumk2.ap-south-1.rds.amazonaws.com",
                                   port="5432",
                                   database="mobile_diagnostics_dev")
